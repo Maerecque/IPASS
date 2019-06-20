1a4682032650704aeb990c6209cf839591ac23ba
Maerecque <marczoomers@gmail.com>
2019-06-20 14:53:42 +0200

    PyOCR link made
    Turns out PyOCR is just trash at seeing text in Natural Images

--- Python/Code/PYOCR.py
+++ Python/Code/PYOCR.py
@@ -9,14 +9,14 @@ if len(tools) == 0:
     print("No OCR tool found")
     sys.exit(1)
 tool = tools[0]
-print("Will use tool '%s'" % (tool.get_name()))
+# print("Will use tool '%s'" % (tool.get_name()))
 
 langs = tool.get_available_languages()
-print("Available languages: %s" % ", ".join(langs))
+# print("Available languages: %s" % ", ".join(langs))
 lang = langs[0]
-print("Will use lang '%s'" % (lang))
+# print("Will use lang '%s'" % (lang))
 
-txt = tool.image_to_string(Image.open('../Images/Text file/Lorem-Ipsum.jpg'),
+txt = tool.image_to_string(Image.open('../Images/Natural Images/LA.jpg'),
                            lang=lang,
                            builder=pyocr.builders.TextBuilder())
 
