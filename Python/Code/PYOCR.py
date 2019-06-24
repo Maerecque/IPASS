fc4f9c44e1fc03c92f7fb9424136e8441c681660
Maerecque <marczoomers@gmail.com>
2019-06-24 15:04:12 +0200

    PYOCR 1.02

--- Python/Code/PYOCR.py
+++ Python/Code/PYOCR.py
@@ -1,23 +1,28 @@
 from PIL import Image
 import sys
 import pyocr
+import cv2
 
 pyocr.tesseract.TESSERACT_CMD = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
 tools = pyocr.get_available_tools()
 if len(tools) == 0:
     print("No OCR tool found")
     sys.exit(1)
 tool = tools[0]
-# print("Will use tool '%s'" % (tool.get_name()))
 
 langs = tool.get_available_languages()
-# print("Available languages: %s" % ", ".join(langs))
-lang = langs[0]
-# print("Will use lang '%s'" % (lang))
+src = '../Images/Natural Images/Meme.jpg'
+img_out = PIL.swt(src, output_type=pillowfight.SWT_OUTPUT_ORIGINAL_BOXES)
 
-txt = tool.image_to_string(Image.open('../Images/Natural Images/LA.jpg'),
-                           lang=lang,
-                           builder=pyocr.builders.TextBuilder())
+def analyse():
+    txt = tool.image_to_string(Image.open(src),
+                               lang=langs,
+                               builder=pyocr.builders.TextBuilder())
+    return txt
 
-print(txt)
+for i in range(len(langs)):
+    if(analyse() == ''):
+        i += 1
+    else:
+        print(analyse())
+        break
