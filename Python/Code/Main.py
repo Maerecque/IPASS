ab977f6307d2322b52d88c14633c1c716224de87
Maerecque <marczoomers@gmail.com>
2019-06-30 11:32:02 +0200

    Menu 1.2
    Made 2 new functions on the menu, 1 to choose another jpg file without having to restart the entire application. 2 a quit button

--- Python/Code/Main.py
+++ Python/Code/Main.py
@@ -8,21 +8,24 @@ from tkinter.filedialog import askopenfilename
 Tk().withdraw()
 filename = askopenfilename(initialdir = '../Images/Natural Images/',title = "Select a .jpg file",filetypes = (("jpeg files","*.jpg"),("all files","*.*"))) # show an "Open" dialog box and return the path to the selected file
 url = filename
-
 top = Tk()
-top.geometry("250x200")
+top.geometry("250x275")
+
 def button_PYOCR():
     OCR = PYOCR(url)
-    top.quit()
     print(OCR.read_text())
 def button_PyTesseract():
     OCR = PyTess(url)
-    top.quit()
     print(OCR.read_text())
 def button_OPENALPR():
     OCR = OPENALPR(url)
-    top.quit()
     print(OCR.read_text())
+
+def button_diff_url(): #function to open a new window with a file choosing menu
+    Tk().withdraw()
+    filename = askopenfilename(initialdir='../Images/Natural Images/', title="Select a .jpg file", filetypes=(("jpeg files", "*.jpg"),("all files", "*.*")))  # show an "Open" dialog box and return the path to the selected file
+    global url
+    url = filename
 
 A = Button(top, text = "Scanned Document Mode", command = button_PyTesseract, width=25)
 A.place(x = 25,y = 25)
@@ -30,4 +33,8 @@ B = Button(top, text = "Natural Image Mode", command = button_PYOCR, width=25)
 B.place(x = 25,y = 75)
 C = Button(top, text = "License Plate Mode", command = button_OPENALPR, width=25)
 C.place(x = 25,y = 125)
+D = Button(top, text = "Choose a different picture", command = button_diff_url, width = 25)
+D.place(x = 25,y = 175)
+E = Button(top, text = "Quit", command = exit, width = 25) #quit application
+E.place(x = 25,y = 225)
 top.mainloop()
