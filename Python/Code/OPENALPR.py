ed6c5fc19a1bbf51c9589a7a4a8778d599c94748
Maerecque <marczoomers@gmail.com>
2019-06-30 14:18:49 +0200

    OPENALPR 1.03
    Added docstring lines to OPENALPR.py

--- Python/Code/OPENALPR.py
+++ Python/Code/OPENALPR.py
@@ -3,7 +3,16 @@ import base64
 import json
 
 class OPENALPR:
+    """
+    This class is for the use of photo's with license plates on them.
+    """
     def __init__(self,image_path):
+        """
+            The constructor for PyTess class.
+
+            Parameters:
+                image_path (string): The path to the chosen jpg file.
+        """
         SECRET_KEY = 'sk_1fe86fdb77300b5e46b613cf'
         COUNTRY = 'us'
         url = 'https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=0&country=%s&secret_key=%s' % (COUNTRY,SECRET_KEY)
@@ -14,6 +23,12 @@ class OPENALPR:
             self.img_base64 = base64.b64encode(image_file.read())
 
     def read_text(self):
+        """
+            The function to extract text from the chosen jpg file.
+
+            Returns:
+                output: If the OCR is able recognise a license plate, then it will return it.
+        """
         r = requests.post(self.url, self.img_base64)
         output = json.loads(r.text)
 
