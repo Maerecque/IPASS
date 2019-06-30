c1a8dafc80daab02bca427564ef77779b4168b98
Maerecque <marczoomers@gmail.com>
2019-06-30 11:30:42 +0200

    PYOCR 1.05
    Fixed bugs, concering not switching font during usage

--- Python/Code/PYOCR.py
+++ Python/Code/PYOCR.py
@@ -18,25 +18,28 @@ class PYOCR:
         self.result = None
 
     def pick_color(self,event,x,y,flags,param):      #https://answers.opencv.org/question/134248/how-to-define-the-lower-and-upper-range-of-a-color/?answer=134284
+        self.i = 0
         if event == cv2.EVENT_LBUTTONDOWN:
-            pixel = image_hsv[y, x]
-            upper = np.array([pixel[0] + 40, pixel[1] + 20, pixel[2] + 40])
-            lower = np.array([pixel[0] - 40, pixel[1] - 20, pixel[2] - 40])
-            image_mask = cv2.inRange(image_hsv, lower, upper)
-            cv2.imwrite(self.temp_src, image_mask)
+            pixel = image_hsv[y, x]                 #take the hsv values of the selected pixel
+            upper = np.array([pixel[0] + 40, pixel[1] + 20, pixel[2] + 40]) #take the most outer hsv value of the selected pixel
+            lower = np.array([pixel[0] - 40, pixel[1] - 20, pixel[2] - 40]) #take the lowest hsv value ofthe selected pixel
+            image_mask = cv2.inRange(image_hsv, lower, upper)               #transform the original picture to a high-contranst mask
+            cv2.imwrite(self.temp_src, image_mask)                          #save the high-contrast picture as temporary jpg file
             for lang in self.langs:
                 if (self.analyse() == ''):
+                    self.i = self.langs.index(lang)
                     print("Using another Font")
+                    print(self.langs[self.i])
                 else:
                     print("Using font:",self.langs[self.i])
                     cv2.destroyAllWindows()
                     self.result = self.analyse()
                     return
 
-    def analyse(self):
+    def analyse(self):                              #try to read text of the current picture
         self.txt = self.tool.image_to_string(Image.open(self.temp_src),
-                                   lang=self.langs[self.i],
-                                   builder=pyocr.builders.TextBuilder())
+                                   lang = self.langs[self.i],
+                                   builder = pyocr.builders.TextBuilder())
         return self.txt
 
     def read_text(self):
